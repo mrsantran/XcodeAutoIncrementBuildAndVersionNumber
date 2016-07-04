14196e0005413c4cb9301cf60ff8c1fdc9533449
Vineet Choudhary <vineetchoudhary@live.in>
2016-07-04 18:44:59 +0530

    Initial Commit

---
+++ AutoVersion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoVersion
+//
+//  Created by Vineet Choudhary on 04/07/16.
+//  Copyright © 2016 VineetChoudhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoVersion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoVersion
+//
+//  Created by Vineet Choudhary on 04/07/16.
+//  Copyright © 2016 VineetChoudhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
